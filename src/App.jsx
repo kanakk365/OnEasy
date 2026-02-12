12a56ae1a9067cc051207b15353fd73b06f5c3ff
Srinath10X <srinathcreates@gmail.com>
2026-02-12 20:09:26 +0530

    chore: console log cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Layout from "./components/layout/Layout";
-import { AUTH_CONFIG } from "./config/auth";
 
 function App() {
-  console.log(AUTH_CONFIG.API_BASE_URL);
-  console.log(AUTH_CONFIG.ENDPOINTS.PHONE_LOGIN);
-
   return (
     <Router>
       <Layout />
