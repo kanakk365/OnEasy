cb826e34dd68ca6a9961f520aa69901f290f16e8
Srinath10X <srinathcreates@gmail.com>
2026-02-12 20:05:47 +0530

    chore: test

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Layout from "./components/layout/Layout";
+import { AUTH_CONFIG } from "./config/auth";
 
 function App() {
+  console.log(AUTH_CONFIG.API_BASE_URL);
+  console.log(AUTH_CONFIG.ENDPOINTS.PHONE_LOGIN);
+
   return (
     <Router>
       <Layout />
